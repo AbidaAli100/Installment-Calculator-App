f96990006831bb64cd986acec32403923cc78da6
AbidaAli100 <abida.ali100@gmail.com>
2021-06-22 23:38:19 +0500

    worked on style and indentation

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function calculate() {
     let amount = parseInt(document.querySelector('#input1').value);
+
     let period = parseInt(document.querySelector('#input2').value);
+
     let monthlyInstallment = amount / period;
-    document.querySelector('span').innerHTML = monthlyInstallment;
+    
+    document.querySelector('span').innerHTML = Math.round(monthlyInstallment);
 }
+
+
